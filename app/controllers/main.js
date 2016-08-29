46b0fff3a16b26d1feef053ec10ffc8e8cf6b375
Ezequiel Rabinovich <erabinovich@gmail.com>
2016-08-29 14:57:10 -0300

    Add random errors

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -3,16 +3,19 @@
 const Boom = require('boom');
 const Controller = require('hapi-routing/controller');
 const DeckManager = require('lib/deck-manager');
+const mightFail = require('lib/random-errors').mightFail;
 
 const manager = new DeckManager();
 
 module.exports = class MainController extends Controller {
   start() {
+    mightFail();
     const token = manager.createDeck();
     this.reply(token);
   }
 
   deal() {
+    mightFail();
     const token = this.request.params.id;
     try {
       const deck = manager.getDeck(token);
---
+++ test/acceptance/random-errors-test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const request = require('request');
+const Server = require('lib/server');
+const randomErrors = require('lib/random-errors');
+
+describe('Server Random errors', () => {
+  const server = new Server();
+  let uri;
+  beforeEach(() => server.start().then(assigned => { uri = assigned; }));
+  afterEach(() => server.stop());
+
+  it('should return a new token when calling POST /deck', done => {
+    randomErrors.every(2);
+    request.post(`${uri}/deck`, (error, response, body) => {
+      expect(error).to.be.null;
+      expect(response.statusCode).to.eq(200);
+      expect(body.length).to.eq(36);
+      request.post(`${uri}/deck`, (error2, response2) => {
+        expect(error2).to.be.null;
+        expect(response2.statusCode).to.eq(500);
+        request.post(`${uri}/deck`, (error3, response3, body3) => {
+          expect(error3).to.be.null;
+          expect(response3.statusCode).to.eq(200);
+          expect(body3.length).to.eq(36);
+          randomErrors.never();
+          done();
+        });
+      });
+    });
+  });
+});
