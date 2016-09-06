43ee61918ba43f68c4e82ec123d96ca3881e0b28
Ezequiel Rabinovich <erabinovich@gmail.com>
2016-09-06 15:44:07 -0300

    Enable random errors on production

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 require('./initialization');
+const config = require('config');
 const Server = require('lib/server');
+const randomErrors = require('lib/random-errors');
 
 const server = new Server(8080);
-
+randomErrors.every(config.failEvery);
 server
   .start()
   .then(uri => console.log(`Listening on ${uri}`));
