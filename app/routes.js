8eb15176e1fcbf7184704f940ab7bdd9e718288f
Ezequiel Rabinovich <erabinovich@gmail.com>
2017-05-19 15:47:53 -0400

    Add healthcheck

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,5 @@
 module.exports = table => table
   .post('/dealer/deck', 'main#start')
   .get('/dealer/deck/{id}/deal/{count?}', 'main#deal')
+  .get('/dealer/health_check', 'main#healthcheck')
   ;
