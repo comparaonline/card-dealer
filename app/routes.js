9b2cf385d9cc75943d427e61392c64375c317ce9
Ezequiel Rabinovich <erabinovich@gmail.com>
2017-06-12 12:09:58 -0400

    Fix environment and update dependencies (#4)
    * Add missing repository info
    
    * Update to npm5
    
    * Improve docker file
    
    * Update hapijs and boomjs
    
    * Update joi
    
    * Update uuid
    
    * Update mocha
    
    * Update eslint
    
    * Fix eslint style
    
    * Add circleci configuration

--- app/routes.js
+++ app/routes.js
@@ -3,5 +3,4 @@
 module.exports = table => table
   .post('/dealer/deck', 'main#start')
   .get('/dealer/deck/{id}/deal/{count?}', 'main#deal')
-  .get('/dealer/health_check', 'main#healthcheck')
-  ;
+  .get('/dealer/health_check', 'main#healthcheck');
